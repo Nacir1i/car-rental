5428ef1d4aa8e06cb71370f9337f80aba2ae3f9c
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-28 23:23:36 +0100

    create basic header

---
+++ components/Header.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { NavItem } from "@nuxt/content/dist/runtime/types";
+
+const navigation = inject<Ref<NavItem[]>>("navigation", ref([]));
+
+const links = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "Collection",
+    to: "/",
+  },
+  {
+    label: "Contact us",
+    to: "/",
+  },
+];
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      <img src="~/public/favicon.ico" class="w-auto h-6" />
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton
+        to="https://github.com/nuxt/ui"
+        target="_blank"
+        icon="i-simple-icons-github"
+        color="gray"
+        variant="ghost"
+      />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template>
+  </UHeader>
+</template>
