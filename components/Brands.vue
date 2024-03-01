fb521a0854143c37d4b789a9e60cf87c6e00ded0
nacir1i <taoufiqnaciri234@gmail.com>
2024-03-01 23:18:01 +0100

    create simple brand and display component

---
+++ components/Brands.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Brand } from "~/types";
+
+defineProps<{
+  brand: Brand;
+}>();
+</script>
+
+<template>
+  <div
+    class="h-40 w-full md:w-40 border border-gray-200 shadow-lg flex justify-center items-center"
+  >
+    <NuxtImg
+      :src="`/images/${brand.toLocaleLowerCase()}.png`"
+      width="90"
+      alt="Brand"
+    />
+  </div>
+</template>
---
+++ components/Display.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Car } from "../types";
+
+defineProps<{ car: Car }>();
+</script>
+<template>
+  <div class="bg-white px-7 py-5 text-center space-y-5">
+    <NuxtImg src="/images/dacia.png" width="200" />
+    <h1 class="text-lg text-black">{{ car.name }}</h1>
+    <h1 class="text-sm text-red-500 font-semibold">{{ car.price }} MAD/Day</h1>
+    <UButton
+      class="rounded-none"
+      to="https://volta.net"
+      target="_blank"
+      color="white"
+      >More details</UButton
+    >
+  </div>
+</template>
