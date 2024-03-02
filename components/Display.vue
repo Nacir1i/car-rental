30d2458675063873ea669495c211816107f26a93
nacir1i <taoufiqnaciri234@gmail.com>
2024-03-02 18:17:21 +0100

    fix buttons url

--- components/Display.vue
+++ components/Display.vue
@@ -8,11 +8,7 @@ defineProps<{ car: Car }>();
     <NuxtImg src="/images/dacia.png" width="200" />
     <h1 class="text-lg text-black">{{ car.name }}</h1>
     <h1 class="text-sm text-red-500 font-semibold">{{ car.price }} MAD/Day</h1>
-    <UButton
-      class="rounded-none"
-      to="https://volta.net"
-      target="_blank"
-      color="white"
+    <UButton class="rounded-none" target="_blank" color="white"
       >More details</UButton
     >
   </div>
