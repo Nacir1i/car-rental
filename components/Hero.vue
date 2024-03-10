cb3b9501bc009005086d76a1f7828d8f3de31b08
nacir1i <taoufiqnaciri234@gmail.com>
2024-03-10 15:55:23 +0000

    update hero section

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="relative mx-auto">
+  <section class="relative mx-auto">
     <NuxtImg
-      width="1400"
-      height="800"
-      class="object-cover w-full h-[420px] lg:h-[560px] xl:h-[640px] skeleton"
+      class="object-cover w-full h-[600px] md:h-[750px] skeleton"
       src="/images/bg.jpg"
       alt="Hero image"
       loading="eager"
@@ -14,23 +12,49 @@
       preload
     />
     <div
-      class="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center p-8"
+      class="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center p-6 gap-7"
     >
-      <h1 class="text-3xl font-bold md:mb-4 md:text-4xl lg:text-6xl">
-        Just landed.
-      </h1>
-      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">
-        The New Year Collection
-      </h2>
-      <div class="max-w-sm mb-8 text-md font-light lg:max-w-md text-balance">
-        <p>
-          Our latest collection is here. Discover the latest trends and styles
-          for the new year.
+      <div class="flex flex-col justify-center items-center gap-3">
+        <div class="text-center">
+          <div class="flex justify-center items-center gap-1">
+            <template v-for="n in 5">
+              <UIcon
+                name="i-heroicons-star-solid"
+                class="text-yellow-400 text-3xl lg:text-5xl"
+              />
+            </template>
+          </div>
+          <h1 class="text-4xl font-bold md:text-6xl lg:text-8xl">
+            Trouvez votre
+          </h1>
+          <h2 class="text-4xl font-bold md:text-6xl lg:text-8xl">
+            Voiture de rêve.
+          </h2>
+        </div>
+        <p class="text-center text-gray-500 text-sm md:text-base lg:text-lg">
+          Nous pouvons vous aider à trouver la meilleure voiture. Consultez nos
+          avis, comparez les modèles et trouvez des voitures à vendre.
         </p>
+      </div>
+      <div class="flex flex-wrap justify-center items-center gap-4">
+        <UButton
+          icon="i-heroicons-magnifying-glass"
+          label="Rechercher un véhicule"
+          color="white"
+          variant="solid"
+          class="text-lg px-5 py-3"
+        />
+        <UButton
+          icon="i-heroicons-check"
+          label="Vendre votre véhicule"
+          color="white"
+          variant="solid"
+          class="text-lg px-5 py-3"
+        />
       </div>
     </div>
     <div
       class="bg-white h-16 w-16 absolute -bottom-8 left-0 right-0 m-auto rotate-45 sm:block hidden"
     ></div>
-  </div>
+  </section>
 </template>
